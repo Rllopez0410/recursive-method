dc1e7c710920397e94a382d3ed72cad924345d12
Roger Lopez <rllopez0410@gmail.com>
2024-11-13 16:19:15 -0500

    created a fuction using a iterator to get a solution of a number the is powered by another number

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//sample problem one
-
-function pow(x ,n) {
-    if (n == 1) {
-        return x;
-    } else {
-        return x * pow(x, n - 1)
-    }
+function pow(x, n) {
+  let result = 1;
+  for (let i = 0; i != n; i++) {
+    result *= x;
+    console.log(result);
+  }
 }
 
-console.log(pow(3, 2))
+pow(2, 4);
+
