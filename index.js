7534d8dd66f29caf6b90831f95ea2298d7615707
Roger Lopez <rllopez0410@gmail.com>
2024-11-14 14:26:31 -0500

    Write a function sumTo(n) that calculates the sum of numbers 1 + 2 + ... + n in bot recursive and iterotively

--- index.js
+++ index.js
@@ -6,5 +6,29 @@ function pow(x, n) {
   }
 }
 
-pow(2, 4);
+function pow(x,n) {
+  if (n == 1) {
+    return x;
+  } else {
+     return x * pow(x, n - 1);
+  }
+}
 
+function SumTo(n) {
+  let num = n;
+  for (let i = 0; i <= n; i++) {
+    n -= 1;
+    num += n;
+    console.log(num);
+  }
+}
+
+function SumToRecur(n) {
+  if (n == 0) {
+    return n;
+  } else {
+    return n + SumToRecur(n - 1);
+  }
+}
+
+console.log(SumToRecur(4))
